6c4e2e0dfd4726b08967678747afa042daf555cc
Jackson W. Martin <jackm2580@gmail.com>
2015-09-08 19:01:00 -0400

    save

--- www/game.js
+++ www/game.js
@@ -1,2 +1,17 @@
-// I am a file
-console.log("Booyeah");
+'use strict';
+
+var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
+  {preload:preload,create:create,update:update,render:render});
+
+function preload() {
+  game.stage.backgroundColor = '#bbbbbb';
+}
+
+function create() {
+}
+
+function update() {
+}
+
+function render() {
+}

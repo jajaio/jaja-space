7e9ddf579f9617e3fee3f9425a3ec72bdebd00bf
Jackson W. Martin <jackm2580@gmail.com>
2015-09-08 19:07:08 -0400

    save

--- game.js
+++ game.js
@@ -3,11 +3,16 @@
 var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
 
+var background;
+
 function preload() {
   game.stage.backgroundColor = '#bbbbbb';
+  game.load.image("background","assets/background.gif");
 }
 
 function create() {
+  background = game.add.tileSprite(0,0,320,568,"background");
+  background.autoScroll(-100,0);
 }
 
 function update() {

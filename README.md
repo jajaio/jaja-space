f27c51e5e74cd15bd1fa202c9dff56bbf5d70e63
Jackson W. Martin <jackm2580@gmail.com>
2015-09-16 08:21:25 -0400

    save

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # game-1
-Games made with Phaser.io
+Games made with the Phaser Engine.
+# Original Bird
+This is just another "Flappy Bird" spin off. So I can learn
+the way this engine works.
--- www/game.js
+++ www/game.js
@@ -18,7 +18,7 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   background = game.add.tileSprite(0,0,320,568,"background");
   background.autoScroll(-100,0);
-  bird = game.add.sprite(20,284,"bird");//10
+  bird = game.add.sprite(10,284,"bird");
   game.physics.enable(bird, Phaser.Physics.ARCADE);
   bird.body.gravity.y = 1000;
 }

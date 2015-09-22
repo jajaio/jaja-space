7f3bf261fb49c487d6093f05ff494c935aacf764
Jackson W. Martin <jackm2580@gmail.com>
2015-09-22 15:04:21 -0400

    save

--- www/game.js
+++ www/game.js
@@ -15,12 +15,18 @@ function preload() {
 }
 
 function create() {
+  //Start Physics Engine
   game.physics.startSystem(Phaser.Physics.ARCADE);
+  //Add our Background
   background = game.add.tileSprite(0,0,320,568,"background");
   background.autoScroll(-100,0);
+  //Add our bird and gice it physics
   bird = game.add.sprite(10,284,"bird");
   game.physics.enable(bird, Phaser.Physics.ARCADE);
   bird.body.gravity.y = 1000;
+  //Add some control to the bird
+  game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+    .onDown.add(function(){bird.body.velocity.y=-200;
 }
 
 function update() {

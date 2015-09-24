980b2b97f684d5264b5ac11a17899a287696c14d
Jackson W. Martin <jackm2580@gmail.com>
2015-09-24 09:29:37 -0400

    save

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-author = "jajaio";
+var author = "jajaio";
 
 var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});

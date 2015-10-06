5b6b4833319d37aa87ee4948a882f3ce055fc745
Jackson W. Martin <jackm2580@gmail.com>
2015-10-06 18:06:58 -0400

    save

--- game.js
+++ game.js
@@ -7,13 +7,15 @@ var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
 
 var background;
 var bird;
-var pipe1t;
+var pipet;
+var pipeb;
 
 function preload() {
   game.stage.backgroundColor = '#bbbbbb';
   game.load.image("background","assets/background.gif");
   game.load.spritesheet("bird","assets/bird.png",80,48);
-  game.load.image("pipe","assets/pipe.gif");
+  game.load.image("pipet","assets/pipet.gif");
+  game.load.image("pipeb","assets/pipeb.gif");
 }
 
 function create() {
@@ -29,7 +31,15 @@ function create() {
   bird.smoothed = false;
   bird.animations.add('fly',[0,1], 6/*6 is the fps*/, true);
   bird.play('fly');
+  //Add The Pipes
+  pipe1t = game.add.sprite(400,30,"pipet");
+  pipe1b = game.add.sprite(400,30,"pipeb");
+  pipe2t = game.add.sprite(600,30);
+  pipe2b = game.add.sprite(600,30);
+  pipe3t = game.add.sprite(800,30);
+  pipe3b = game.add.sprite(800,30);
   //Physics
+  //
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.physics.enable(bird, Phaser.Physics.ARCADE);
   bird.body.gravity.y = 1000;
@@ -39,6 +49,9 @@ function create() {
   game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(flap); //The Higher the Negative number, the higher our bird will jump.  
 }
 
+function startPipe(){
+
+}
 function update() {
 }
 

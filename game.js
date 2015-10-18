1410ba82bd25bd6d384620149ad5b95525bb1b68
Jackson W. Martin <jackm2580@gmail.com>
2015-10-18 15:31:59 -0400

    save

--- game.js
+++ game.js
@@ -29,7 +29,7 @@ function create() {
   }
   centery=game.height/2; //centery is center y
   //Add our Background
-  background = game.add.tileSprite(0,0,320,568,"background");
+  background = game.add.tileSprite(0,0,3200,568,"background");
   background.autoScroll(-100,0);
   //Add our bird and give it physics
   bird = game.add.sprite(10,284,"bird");
